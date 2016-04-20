405a0838cb7de58d6f012a2a47525ddfba3e39ff
Ryan McGuire <ryan@enigmacurry.com>
2016-04-19 20:53:25 -0400

    Replaces scan tool with something simpler.

--- scan.sh
+++ scan.sh
@@ -1,26 +1,2 @@
 #!/bin/bash
-
-# Remember all hosts seen by name:
-declare -A hosts
-
-while true; do
-    avahi_hosts=$(avahi-browse _archiso._tcp --resolve -t 2>&1 | grep address | sed -r -n 's/address = \[(.*)\]/\1/p' | sort -u)
-    saw_new=n
-    for h in $avahi_hosts; do
-	if [ "${hosts[$h]}" != "y" ]; then
-	    echo `date` - New archiso instance found: $h
-	    saw_new=y
-	fi
-    done
-    if [ $saw_new == y ]; then
-	echo "--"
-    fi
-
-    # Hosts may also have gone away, so reset hosts to just the new hosts:
-    unset hosts
-    declare -A hosts
-    for h in $avahi_hosts; do
-	hosts[$h]="y"
-    done
-    sleep 1
-done
+avahi-browse -p -a --resolve | awk -F ';' '/archiso/{print $8}'
